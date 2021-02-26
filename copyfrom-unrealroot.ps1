6f3945a969b63337d05496d189150a33aaf91c78
Andreé Hallengren <andree.hallengren@embark-studios.com>
2021-02-26 15:33:59 +0100

    copyfrom and copyto scripts

---
+++ copyfrom-unrealroot.ps1
@@ -0,0 +1,19 @@
+# Copies the Plugins\Marketplace folder under $ueroot to here.
+# $ueroot is the Unreal code root folder (the one which contains e.g. Engine and LICENSE.md)
+param (
+	[Parameter(Mandatory = $true)]
+	[string]$ueroot
+)
+
+$pluginName = "Agones"
+
+$pluginRoot = join-path $ueroot "Engine\Plugins\Marketplace\$pluginName\sdks\unreal"
+
+$files = git ls-tree -r --name-only embark
+$files | foreach {
+	$dest = $_
+	$src = (join-path $pluginRoot $dest)
+	if ((test-path $dest -PathType Leaf) -and (test-path $src)) {
+		copy-item $src $dest
+	}
+}
