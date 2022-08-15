933c5cfa9ec8f2de41413341dcf9e0311aceb3d7
Andreé Hallengren <andree.hallengren@embark-studios.com>
2022-08-15 13:15:18 +0200

    add copyto and copyfrom

---
+++ copyto-unrealroot.ps1
@@ -0,0 +1,36 @@
+# Copies the workspace to the Plugins\Marketplace folder under $ueroot.
+# $ueroot is the Unreal code root folder (the one which contains e.g. Engine and LICENSE.md)
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$ueroot
+)
+
+$pluginName = "Agones"
+$pluginRoot = join-path $ueroot "Engine\Plugins\Marketplace\$pluginName"
+
+$files = (git ls-tree -r --name-only "embark:sdks/unreal/Agones") | Select-String "\.ps1$" -NotMatch
+$files | foreach {
+    $file = $_
+    $src = "sdks/unreal/Agones/" + $file
+    if (test-path $src -PathType Leaf) {
+        $dest = (join-path $pluginRoot $file)
+        (New-Item -Path (Split-Path -Path $dest) -Type Directory -ErrorAction SilentlyContinue)
+        copy-item $src $dest
+    }
+}
+
+$sha = (git rev-parse HEAD)
+$origin = (git remote get-url --push origin)
+$branch = (git branch --show-current)
+
+$infoPath = (join-path $pluginRoot "PLUGIN_ORIGIN.json")
+if (Test-Path $infoPath) {
+    remove-item $infoPath
+}
+
+$pluginOrigin = New-Item -ItemType File -Path $infoPath
+"{" >> $pluginOrigin
+"  `"origin`": `"$origin`"," >> $pluginOrigin
+"  `"branch`": `"$branch`"," >> $pluginOrigin
+"  `"commit`": `"$sha`"" >> $pluginOrigin
+"}" >> $pluginOrigin
